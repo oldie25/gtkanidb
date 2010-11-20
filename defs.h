77f9af58060e399819d66c8e86a477e1d947d2cd
Jayson Oldfather <oldie25@gmail.com>
2010-11-20 11:42:29 -0600

    remove files
    these files are no longer in use in the project

--- defs.h
+++
@@ -1,9 +0,0 @@
-#ifndef __defs_file_h_included
-#define __defs_file_h_included
-
-extern const char* ANIDBSERVER;
-extern const int REMOTEPORT;
-extern const int LOCALPORT;
-extern const char* SAFETYFILE;
-
-#endif
